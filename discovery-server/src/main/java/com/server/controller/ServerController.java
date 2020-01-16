132970b9ba53e8e9bfa34b7242185ac9b7b47182
mafei <mafei.dev@gmail.com>
2020-01-16 17:31:29 +0530

    0

---
+++ discovery-server/src/main/java/com/server/controller/ServerController.java
@@ -0,0 +1,17 @@
+package com.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/server")
+public class ServerController {
+
+    @GetMapping("/info")
+    public ResponseEntity<?> index() {
+        return new ResponseEntity<>("HI", HttpStatus.OK);
+    }
+}
