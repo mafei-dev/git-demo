ae6aca27aa5c06bc2d46a6888de56f39f22e03ce
mafei <mafei.dev@gmail.com>
2020-01-17 15:26:58 +0530

    basic security

--- user-service/src/main/java/com/user/security/SecurityConfig.java
+++ user-service/src/main/java/com/user/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.user.security;
 
+/*
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+*/
 
 //@Configuration
 //@EnableWebSecurity
